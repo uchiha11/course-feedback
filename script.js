797b2af87b720322a6d36ad353489b24848e4024
psomappa <psomappa@671211551nb002.lan>
2019-07-17 13:27:48 +0530

    branch1 changes

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 var stored = 0;
 var count = 0;
-
-function dis(value) {
+var i = 0;
+function dis(value,question) {
     count++;
+   
+    var val = {};
 
-    var val = [];
-
-    val[0] = value;
+    val[question] = value;
     localStorage.setItem("values", JSON.stringify(val));
+
 
 
     var storedValues = JSON.parse(localStorage.getItem("values"));
 
-    storedValues = parseInt(storedValues);
-    stored = stored + storedValues;
-    console.log("current rating is " + storedValues);
+    storedValues[i] = parseInt(storedValues);
+    stored = stored + storedValues[i];
+    console.log("current rating is " + storedValues[i]);
+    i++;
 
 }
 
@@ -23,23 +25,42 @@ function submit() {
     var page = document.getElementById('form').dataset.page;
 
     var rating = stored / count;
-    if (rating <= 1) 
+    if (rating <= 1)
         alert(`Overrall rating for course ${page} is Poor . ${rating}`);
-    
+
     else if (rating > 1 && rating <= 2)
         alert(`Overrall rating for course ${page} is intermediate . ${rating}`);
     else if (rating > 2 && rating <= 3)
         alert(`Overrall rating for course ${page} is Good`);
 
-    else alert(`Overrall rating for course ${page} is Excellent . ${rating}`);
+    else alert(`Overrall rating for course is Excellent .`);
 
-   alert(comments);
+    alert(comments);
 
-      }
+}
 
-      function login() {
-        window.location.href = './login.html';
-        var name=document.getElementById('icon_prefix').value;
-        var email=document.getElementById('icon_email').value;
-        console.log(` My name is ${name} and my email ${email}`);
-      }
+function regExp(x) {
+
+    var reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    return reg.test(x);
+}
+
+
+function login() {
+
+    var name = document.getElementById('icon_prefix').value;
+    var email = document.getElementById('icon_email').value;
+    if(name==="")
+    alert("write valid name")
+
+    var m=regExp(email);
+         if(m){
+
+ window.location.href = './login.html';
+
+         }
+         else 
+         alert("Email id must be valid")
+
+ 
+}
