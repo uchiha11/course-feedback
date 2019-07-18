20ff7c58decac6db2ebab4c09bd304a70c83ed36
psomappa <psomappa@671211551nb002.lan>
2019-07-18 10:34:18 +0530

    final

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 
 var val = {};
 
-function get(){
-return JSON.parse(localStorage.getItem('feedback')) || {}
+function get() {
+    return JSON.parse(localStorage.getItem('feedback')) || {}
 }
 
-function set(key,value){
+function set(key, value) {
 
-    var obj= get();
+    var obj = get();
     obj[key] = value;
-    localStorage.setItem('feedback',JSON.stringify(obj))
+    localStorage.setItem('feedback', JSON.stringify(obj))
 
 }
 function dis(value, question) {
 
-if(question=='sdlc'){
-    set('sdlc',value);
-}
-
-else if(question=='pdlc'){
-    set('pdlc',value);}
-
-   else if(question=='qa'){
-        set('qa',value);
+    if (question == 'sdlc') {
+        set('sdlc', value);
     }
-        
+
+    else if (question == 'pdlc') {
+        set('pdlc', value);
+    }
+
+    else if (question == 'qa') {
+        set('qa', value);
+    }
+
 
 }
 
 function submit() {
     var comment = document.getElementById('icon_prefix2').value;
     var page = document.getElementById('form').dataset.page;
-  set('comment',comment);
-  
-function download(text, name, type) {
-    var a = document.createElement("a");
-    var file = new Blob([text], {type: type});
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-    document.body.appendChild(a); 
-    a.click();
-  }
+    set('comment', comment);
 
-  download(JSON.stringify(get()),'test.txt','txt');
+    function download(text, name, type) {
+        var a = document.createElement("a");
+        var file = new Blob([text], { type: type });
+        a.href = URL.createObjectURL(file);
+        a.download = name;
+        document.body.appendChild(a);
+        a.click();
+    }
+
+    download(JSON.stringify(get()), 'test.txt', 'txt');
 }
 
 function regExp(x) {
@@ -62,8 +63,8 @@ function login() {
 
     var m = regExp(email);
 
-   set('name',name);
-   set('email',email);
+    set('name', name);
+    set('email', email);
 
 
     if (m) {
